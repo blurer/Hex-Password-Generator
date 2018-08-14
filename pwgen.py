396ba670d29cdf7d2c71d68d13fd6300620fc266
blurer <40605557+blurer@users.noreply.github.com>
2018-08-14 08:39:19 -0400

    Add files via upload

---
+++ pwgen.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+print('')
+print('HEX PASSWORD GENERATOR')
+print('')
+
+#import csv
+import random
+#import string
+
+chars = 'abcdefABCDEF1234567890'
+
+length = input('HOW LONG: ')
+length = int(length)
+
+qty = input('HOW MANY: ')
+qty = int(qty)
+
+
+for y in range(qty):
+    password = ''
+    for c in range(length):
+        password += random.choice(chars)
+    print(password)
